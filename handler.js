4f13c4bdebeea29745dc77873f50c42a4e27d4fc
usama9999 <usamashahbaz999@gmail.com>
2023-07-15 16:35:09 +0500

    .

--- handler.js
+++ handler.js
@@ -13,6 +13,14 @@ app.get("/hello", function (req, res) {
     return res.send(results);
   });
 });
+app.get("/usama", function (req, res) {
+  connection.query("select * from users", function (err, results, fields) {
+    if (err) {
+      return res.send(err);
+    }
+    return res.send(results);
+  });
+});
 const PORT = 5000;
 app.listen(PORT, "0.0.0.0", function () {
   console.log(`App Listening on port ${PORT}`);
